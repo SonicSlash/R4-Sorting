0162129892a73f9b679387f9b868429be15d41e7
SonicSlash <VladimirTeresh@mail.ru>
2015-10-06 23:32:33 +0300

    Yummm

---
+++ Bubble.java
@@ -0,0 +1,30 @@
+//: object/HelloData.java
+package com.company;
+
+
+public class Bubble{
+    public static void main(String[] args) {
+        String arr[] = {"Hi", "hatty", "you", "are", "well", "come", "to", "my", "smoke", "freak"};
+        PrintArr(arr);
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j].compareToIgnoreCase(arr[j + 1]) > 0) {
+                    swap(arr, j, j + 1);
+                    }
+            }
+        }
+        PrintArr(arr);
+    }
+
+    public static void swap(String[] array, int i, int j) {
+        String temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    public static void PrintArr(String[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+}
