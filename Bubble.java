1d99751b7c3a37b0c3934416c7f36d40f78932c4
SonicSlash <VladimirTeresh@mail.ru>
2015-10-08 23:41:40 +0300

    Все_поправил)))

--- Bubble.java
+++ Bubble.java
@@ -1,27 +1,25 @@
-//: object/HelloData.java
-package com.company;
+package ru.sonic;
 
 
 public class Bubble{
     public static void main(String[] args) {
         String arr[] = {"Hi", "hatty", "you", "are", "well", "come", "to", "my", "smoke", "freak"};
-        PrintArr(arr);
+        printArr(arr);
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
                 if (arr[j].compareToIgnoreCase(arr[j + 1]) > 0) {
                     swap(arr, j, j + 1);
-                    }
+                }
             }
         }
-        PrintArr(arr);
+        printArr(arr);
     }
-
     public static void swap(String[] array, int i, int j) {
         String temp = array[i];
         array[i] = array[j];
         array[j] = temp;
     }
-    public static void PrintArr(String[] array) {
+    public static void printArr(String[] array) {
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
